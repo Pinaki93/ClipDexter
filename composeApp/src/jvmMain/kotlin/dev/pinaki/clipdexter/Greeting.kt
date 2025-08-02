3723fe992260c0b5c82d1015cdee68a9bddc81c7
pinakiacharya <pinakiacharyagd@gmail.com>
2025-08-02 21:25:01 +0530

    Initial commit: Kotlin Multiplatform project setup

---
+++ composeApp/src/jvmMain/kotlin/dev/pinaki/clipdexter/Greeting.kt
@@ -0,0 +1,9 @@
+package dev.pinaki.clipdexter
+
+class Greeting {
+    private val platform = getPlatform()
+
+    fun greet(): String {
+        return "Hello, ${platform.name}!"
+    }
+}
---
+++ composeApp/src/jvmTest/kotlin/dev/pinaki/clipdexter/ComposeAppDesktopTest.kt
@@ -0,0 +1,12 @@
+package dev.pinaki.clipdexter
+
+import kotlin.test.Test
+import kotlin.test.assertEquals
+
+class ComposeAppDesktopTest {
+
+    @Test
+    fun example() {
+        assertEquals(3, 1 + 2)
+    }
+}
